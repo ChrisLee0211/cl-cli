de3a64bb75686f69dbbfc50db24bb7569bf65a84
李铭辉 <1018596741@qq.com>
2020-11-19 23:15:23 +0800

    feat: 增加bin入口逻辑

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
+import program = require('commander')
+import {initProject} from '../core/init/index';
 
+// 设置版本号和参数，通过 mycli --help 查看
+program.version('1.0.0')
+    .option('-t, --type <name>', 'project type')
+    .option('-n, --name <name>', 'project type')
+
+// 捕获命令和参数 eg： mycli create test -t vue
+program
+    .command('create <name>')
+    .action(function(name) {
+        initProject(name)
+    })
+
+program.parse(process.argv)
