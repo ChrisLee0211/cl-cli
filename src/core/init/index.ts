c214b824174f82d484e963f15e378f618c8d45c0
李铭辉 <1018596741@qq.com>
2020-11-20 23:24:07 +0800

    doc: 整理开发需求

--- src/core/init/index.ts
+++ src/core/init/index.ts
@@ -1,7 +1,15 @@
 import inquirer from 'inquirer';
 import {log} from '../../utils/log';
+import * as fs from 'fs';
+import {getCurrentPath} from '../../utils/path';
 
-
-export const initProject = (name:string) => {
-
+/**
+ * 初始化项目信息
+ * @param name 项目名称
+ * @author chris lee
+ * @Time 2020/11/20
+ */
+export const initProject = async (name:string):Promise<void> => {
+    const path = getCurrentPath();
+    
 }
