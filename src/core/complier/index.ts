e225a7bad00e7e56f8a1abed91cbe534d5a96676
李铭辉 <1018596741@qq.com>
2021-01-18 23:05:54 +0800

    feat: 完成fileTree深度遍历生成基础逻辑

--- src/core/complier/index.ts
+++ src/core/complier/index.ts
@@ -1,14 +1,15 @@
 import utils, { fileNode } from "../helpers/UtilsLib";
 import * as path from 'path';
 import * as fs from 'fs';
-import {readFileContent} from '../../utils/file';
+import {readFileContent,scanFolder} from '../../utils/file';
+import {Stack} from '../../utils/stack'
 
 interface CoreComplierInterface {
     fileTree : fileNode | undefined,
-    /** 创建fileNode */
-    // createFileNode(name:string,path?:string,content?:any,isFolder?:boolean):fileNode
+    /** 构建基础fileNode */
+    // createBaseFileNode(pathName:string):fileNode
     /** 将本地拉取的模版目录编译成fileTree */
-    complierLocalTemplate(pathName:string):void;
+    complierLocalTemplate(path):void;
     /** 将传入的fileList依次插入到fileTree */
     complierExtra(fileList:fileNode[]):void
     /** 将fileTree生成为真实文件 */
@@ -18,8 +19,9 @@ interface CoreComplierInterface {
 
 
 export default class CoreComplier implements CoreComplierInterface{
-    fileTree;
+    fileTree:fileNode | undefined = undefined;
     constructor(path:string){
+        this.fileTree = this.createBaseFileNode(path);
         this.complierLocalTemplate(path)
     }
 
@@ -29,34 +31,15 @@ export default class CoreComplier implements CoreComplierInterface{
      * @author chris lee
      * @Time 2021/01/14
      */
-    private createBaseFileNode(pathName){
-        return new Promise((rs,rj) => {
-            fs.readdir(pathName,{withFileTypes:true},(err,files) => {
-                if(!err){
-                   const rootFileNode = utils.createFileNode(
-                       path.basename(pathName),
-                       pathName,
-                       pathName,
-                       null,
-                       true,
-                       ) 
-                   const len = files.length;
-                   for(let i=0;i<len;i++){
-                    const curPath = pathName;
-                    const rootPath = pathName;
-                    const isFolder = files[i].isDirectory();
-                    const fileName = files[i].name;
-                    const parent = rootFileNode;
-                    const curFileNode = utils.createFileNode(fileName,curPath,rootPath,null,isFolder,parent);
-                    rootFileNode.children.push(curFileNode)
-                   }
-                   rs(rootFileNode)
-                }else{
-                    console.error(err)
-                    rj(err)
-                }
-            })
-        })
+    private createBaseFileNode(pathName):fileNode{
+        const rootFileNode = utils.createFileNode(
+            path.basename(pathName),
+            pathName,
+            pathName,
+            null,
+            true,
+            );
+        return rootFileNode
     }
 
     /**
@@ -67,7 +50,42 @@ export default class CoreComplier implements CoreComplierInterface{
      */
     async complierLocalTemplate(pathName:string){
         try{
-            this.fileTree = await this.createBaseFileNode(pathName)
+            const stack = new Stack();
+            if(this.fileTree!==undefined){
+                stack.push(this.fileTree);
+                while(stack.length>0){
+                    const curNode = stack.pop() as fileNode;
+                    if(curNode.isFolder){
+                        // 如果是文件夹类型，那么先创建一个不含content的fileNode完成树结构，等下一轮遍历再补全content
+                        const files = await scanFolder(pathName);
+                        if(files.length){
+                            const len = files.length;
+                            for(let i=0;i<len;i++){
+                                const curPath = path.join(curNode.path,files[i].name);
+                                const rootPath = pathName;
+                                const isFolder = files[i].isDirectory();
+                                const fileName = files[i].name;
+                                const parent = curNode;
+                                const curFileNode = utils.createFileNode(fileName,curPath,rootPath,null,isFolder,parent);
+                                curNode.children.push(curFileNode);
+                                stack.push(curFileNode)
+                            }
+                        }
+                    }else{
+                        // 如果不是文件夹类型，那么就开始尝试读取content
+                        if(curNode.content === null){
+                            try{
+                                curNode.content = await readFileContent(curNode.path);
+                            }catch(e){
+                                console.error(e)
+                            }
+                        }
+                        
+                    }
+                }
+            }else{
+                throw new Error(`Fail to complier local template`)
+            }
         }catch(e){
             console.error(e)
         }
