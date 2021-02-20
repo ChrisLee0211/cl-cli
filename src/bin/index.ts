39be753ce598b13663ef76fea16d52121c9cec85
李铭辉 <1018596741@qq.com>
2021-02-20 17:52:18 +0800

    feat: 增加配置文件式自定义脚手架

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -6,15 +6,17 @@ import {basePlugin, uiPlugin, framePlugin} from "../core/plugins/self";
 
 // 设置版本号和参数，通过 mycli --help 查看
 program.version("1.0.0")
-    .option("-t, --type <name>", "project type")
-    .option("-n, --projectName <name>", "project name")
-    .option("-p --plugin <name>", "plugin config");
 
 // 捕获命令和参数 eg： mycli create test -t vue
 program
     .command("create <name>")
-    .action(function(name) {
-        // initProject(name)
+    .option("-p --pluginConfig <name>", "plugin config")
+    .action(function(name,cmd) {
+        // 通过插件配置文件方式生成脚手架
+        if(Object.keys(cmd).includes('pluginConfig')){
+            const pluhinConfigPath = cmd.plugin;
+            // 通过读取文件执行ins.use
+        }
         const ins = new ClCore();
         ins.use(basePlugin).use(framePlugin).use(uiPlugin);
         ins.createCli(name);
--- test/index.ts
+++ test/index.ts
@@ -5,21 +5,23 @@ import * as fs from "fs";
 import {basePlugin, uiPlugin, framePlugin} from "../src/core/plugins/self";
 
 // // 设置版本号和参数，通过 mycli --help 查看
-// program.version('1.0.0')
-//     .option('-t, --type <name>', 'project type')
-//     .option('-n, --projectName <name>', 'project type')
+program.version('1.0.0')
+    .option('-t, --type <name>', 'project type')
+    .option('-n, --projectName <name>', 'project type')
+// 捕获命令和参数 eg： mycli create test -t vue
+program
+    .command('create <name>')
+    .option("-p --pluginConfig <name>", "plugin config")
+    .action(function(name,command) {
+        // initProject(name)
+        console.log('project name',name);
+        console.log('plugin config',command)
+        // const ins = new ClCore();
+        // ins.use(basePlugin).use(framePlugin).use(uiPlugin);
+        // ins.createCli(`testFile`);
+    })
 
-// // 捕获命令和参数 eg： mycli create test -t vue
-// program
-//     .command('create <name>')
-//     .action(function(name) {
-//         // initProject(name)
-//         const ins = new ClCore();
-//         ins.use(basePlugin).use(framePlugin).use(uiPlugin);
-//         ins.createCli(`testFile`);
-//     })
-
-// program.parse(process.argv)
+program.parse(process.argv)
 
 const ins = new ClCore();
 // const curPath = process.cwd();
