c07275d074bb552eb09b63d2ab76d4c6aede9764
李铭辉 <1018596741@qq.com>
2021-03-16 17:35:11 +0800

    upd: 补充fileTree内容

--- src/core/fileTree/index.ts
+++ src/core/fileTree/index.ts
@@ -1,8 +1,25 @@
 import fileNode from '../fNode/main';
 
-class fileTree {
+interface fileTree {
+    findById(id:string):fileNode | undefined
+    findByName(name:string):fileNode[]
+    writeById(id:string, content): Promise<boolean>
+}
+class fileTreeCtr implements fileTree {
     tree:fileNode;
     constructor(root:fileNode){
         this.tree = root
+        this.findById.bind(this)
+        this.findByName.bind(this)
+        this.writeById.bind(this)
+    }
+    findById(id:string):fileNode | undefined{
+
+    }
+    findByName(name:string):fileNode[] {
+
+    }
+    writeById(id:string, content): Promise<boolean> {
+
     }
 }
